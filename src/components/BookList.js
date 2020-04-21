fbcf2bf5e78914fe258a19b5495b6ffa02d2cac1
geraldgsh <geraldgsh@hotmail.com>
2020-04-21 23:45:46 +0800

    setup booklist component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import Book from "Books.js";
 
 const BookList = () => {
   return (
-    <div></div>
+    <table>
+      <thead>
+        <tr>
+          <th>Book ID</th>
+        </tr>
+        <tr>
+          <th>Title</th>
+        </tr>
+        <tr>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.values(books).map((book) => (
+          <Book id={book.id} title={book.title} category={book.category} />
+        ))}
+      </tbody>
+    </table>
   );
 };
 
-export default BookList
+function mapStateToProps(state) {
+  return {
+    books: state.books,
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
