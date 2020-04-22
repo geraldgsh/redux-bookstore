e6c76fef0179be692ee0ed2f12498dbf0eb84eed
Aaron Rory Newbold <aaron.rory.newbold@gmail.com>
2020-04-22 13:22:37 -0400

    Removed console.log statements

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,8 +1,6 @@
 import { CHANGE_FILTER } from '../actions/index';
 
 const filterReducer = (state = 'All', action) => {
-  console.log(state);
-  console.log(action);
   switch (action.type) {
     case CHANGE_FILTER:
       return action.category;
